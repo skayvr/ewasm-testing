52221e2c381e7402b849ed6ea341d08a55377ee9
James <55668006+skayvr@users.noreply.github.com>
2021-06-19 20:19:22 -0700

    Adding target-docker app directory and script to start docker.

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo docker run -i -t ethereum/cpp-build-env:15-clang-10 /bin/bash
+echo "Remember: This must be run on the host machine!"
+
+target_directory=$(pwd)
+target_directory=${target_directory#"/host"}
+target_directory="${target_directory/users/Users}" 
+
+# Unfortunately can't use $(pwd) due to setup constraints.
+sudo docker run -i -t --mount type=bind,source="$target_directory"/target-docker,target=/target-docker ethereum/cpp-build-env:15-clang-10 /bin/bash
 
 # Mount directories must be on the host machine.
 #sudo docker run -i -t --mount type=bind,source="$(pwd)"/docker-target,target=/app ethereum/cpp-build-env:15-clang-10 /bin/bash
---
+++ target-docker/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# https://stackoverflow.com/questions/16365130/what-is-the-difference-between-usr-bin-env-bash-and-usr-bin-bash
+
+echo CXX: $CXX
+echo Generator: $GENERATOR
+cmake --version
