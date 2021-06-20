3c67eaf2c9aba61b223b271b5d3133038b202232
James <55668006+skayvr@users.noreply.github.com>
2021-06-19 22:40:30 -0700

    Attempt to build hera.

--- run-docker.sh
+++ run-docker.sh
@@ -7,7 +7,7 @@ target_directory=${target_directory#"/host"}
 target_directory="${target_directory/users/Users}" 
 
 # Unfortunately can't use $(pwd) due to setup constraints.
-sudo docker run -i -t --mount type=bind,source="$target_directory"/target-docker,target=/target-docker ethereum/cpp-build-env:15-clang-10 /bin/bash
+sudo docker run -i -t --mount type=bind,source="$target_directory"/target-docker,target=/target-docker ethereum/cpp-build-env:15-clang-9 /bin/bash
 
 # Mount directories must be on the host machine.
 #sudo docker run -i -t --mount type=bind,source="$(pwd)"/docker-target,target=/app ethereum/cpp-build-env:15-clang-10 /bin/bash
--- target-docker/build.sh
+++ target-docker/build.sh
@@ -4,3 +4,16 @@
 echo CXX: $CXX
 echo Generator: $GENERATOR
 cmake --version
+
+CXX=clang++
+
+pushd hera
+
+mkdir build
+
+pushd build
+cmake -DBUILD_SHARED_LIBS=ON ..
+cmake --build .
+popd
+
+popd
