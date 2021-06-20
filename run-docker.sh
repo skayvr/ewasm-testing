4e4d29bc1f61d6893c5ab45e84eeb6b767ae9684
James <55668006+skayvr@users.noreply.github.com>
2021-06-19 23:15:13 -0700

    Adding notes to run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -9,6 +9,9 @@ target_directory="${target_directory/users/Users}"
 # Unfortunately can't use $(pwd) due to setup constraints.
 sudo docker run -i -t --mount type=bind,source="$target_directory"/target-docker,target=/target-docker ethereum/cpp-build-env:15-clang-9 /bin/bash
 
+# cpp-build-env tags that can be used in the docker file.
+# https://hub.docker.com/r/ethereum/cpp-build-env/tags?page=1&ordering=last_updated
+
 # Mount directories must be on the host machine.
 #sudo docker run -i -t --mount type=bind,source="$(pwd)"/docker-target,target=/app ethereum/cpp-build-env:15-clang-10 /bin/bash
 
